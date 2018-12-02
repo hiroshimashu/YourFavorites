86e123e70027ca7d8365542c9bdff606c20fae76
hiroshimashu <hiroshimashu@gmail.com>
2018-12-02 23:29:41 +0900

    first commit

---
+++ YourFavorites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourFavorites
+//
+//  Created by Hiroshi Imamasu on 2018/12/02.
+//  Copyright © 2018 Hiroshi Imamasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
